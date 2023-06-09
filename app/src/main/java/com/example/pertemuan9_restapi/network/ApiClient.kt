b7fd2ce272160698cbcb47fd26e62985bdbaeb16
andreasandi17010 <20102017@ittelkom-pwt.ac.id>
2023-06-09 08:41:00 +0700

    Pertemuan9_RestApi2

--- app/src/main/java/com/example/pertemuan9_restapi/network/ApiClient.kt
+++ app/src/main/java/com/example/pertemuan9_restapi/network/ApiClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
-    const val BASE_URL = "https://apitani.burunghantu.id/"
+    const val BASE_URL = "https://apitani.burunghantu.id/public/"
 
     val instance : ApiService by lazy {
         val retrofit = Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).build()
